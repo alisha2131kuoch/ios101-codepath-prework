dd21c4cfacc26972c93d3713e7770d355cdbe1fd
alisha2131kuoch <alishakuoch@gmail.com>
2025-08-06 14:24:25 -0700

    Add files via upload

---
+++ Test/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Alisha Kuoch on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        
+        func changeColor() -> UIColor{
+
+               let red = CGFloat.random(in: 0...1)
+               let green = CGFloat.random(in: 0...1)
+               let blue = CGFloat.random(in: 0...1)
+
+               return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+           }
+        
+        let randomColor = changeColor()
+            view.backgroundColor = randomColor
+    }
+    
+}
+
